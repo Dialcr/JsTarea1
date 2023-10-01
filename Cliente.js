fc42439e2df0881d4e05fe64e538f1993f4c81fd
Diego Alfonso Criado <diego.ac@factory.generalsoftwareinc.com>
2023-10-01 00:03:24 -0400

    add client address and getInformation fuinction

--- Cliente.js
+++ Cliente.js
@@ -1,26 +1,32 @@
-export default class Cliente
+export default class Client
 {
-    constructor(name, email, phone, id)
+    constructor(name, email, phone,address)
     {
-          this._id = id;
-           let _name = name;
-           let _email = email;
-           let _phone = phone;
+         let _name = name;
+         let _email = email;
+         let _phone = phone;
+         let _address = address;
 
-           this.getId = () => _id;
-           this.getName = () => _name;
-           this.getEmail = () => _email;
-           this.getPhone = () => _phone;
+          this.getAddress = () => _address;
+          this.getName = () => _name;
+          this.getEmail = () => _email;
+          this.getPhone = () => _phone;
 
-           this.setEmail = function(newEmail)
-           {
-                _email = newEmail;
-           }
-           this.setPhone = function(newPhone)
-           {
-                _phone = newPhone;
-           }
-           
+          this.setEmail = function(newEmail)
+          {
+               _email = newEmail;
+          }
+          this.setPhone = function(newPhone)
+          {
+               _phone = newPhone;
+          }
+          this.setAddress = function(address)
+          {
+               _address = address;
+          }
+
+          this.getInformation = () => `${_name} ${_email} ${_phone} ${_address}`;
+          
     }
 
 
